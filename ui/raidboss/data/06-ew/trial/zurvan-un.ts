fe44ced3978dc1a8effa844dcea1f0d1079ecf7a
ksh local <kshman@msn.com>
2024-01-17 15:44:04 +0900

    즈르반 / 근육산 싱크

--- ui/raidboss/data/06-ew/trial/zurvan-un.ts
+++ ui/raidboss/data/06-ew/trial/zurvan-un.ts
@@ -434,7 +434,6 @@ const triggerSet: TriggerSet<Data> = {
     },
     {
       'locale': 'ja',
-      'missingTranslations': true,
       'replaceSync': {
         'Execrated Wile': 'テンパード・ワイル',
         'Zurvan': 'ズルワーン',
