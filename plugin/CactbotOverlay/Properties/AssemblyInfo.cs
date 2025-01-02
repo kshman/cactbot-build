9b936446064c6ca483fe924585ddf526e610ea05
ksh local <kshman@msn.com>
2025-01-02 19:18:46 +0900

    138

--- plugin/CactbotOverlay/Properties/AssemblyInfo.cs
+++ plugin/CactbotOverlay/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 // - Build Number
 // - Revision
 // GitHub has only 3 version components, so Revision should always be 0.
-[assembly: AssemblyVersion("99.137.0.0")]
-[assembly: AssemblyFileVersion("99.137.0.0")]
+[assembly: AssemblyVersion("99.138.0.0")]
+[assembly: AssemblyFileVersion("99.138.0.0")]
